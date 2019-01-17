de2fac6d654719a51247023ac5e7d2c34ceae06c
Ella <ellazhang37@gmail.com>
2019-01-17 13:23:11 -0500

    Merge Sort

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 9, 3, 7, 4, 1 };
+            MergeSorting(array);
+            //Merging(array);
+        }
+
+        private static void MergeSorting(int[] array)
+        {
+            
+        }
+    }
+}
