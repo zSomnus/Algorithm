72a8970c1c49179e3b3c5c9708eaa270e3aed20d
Ella Zhang <ellazhang37@gmail.com>
2019-01-27 14:49:20 -0500

    + Completed Quicksort
    27 - Jan - 2019

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -6,9 +6,9 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
-            int[] A = { 9, 3, 6, 8, 1, 4 };
-            //Partition(A, 0, 5);
-            Quicksorting(A, 0, 5);
+            int[] A = { 13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11 };
+            //int[] A = { 2, 8, 7, 1, 3, 5, 6, 4 };
+            Quicksorting(A, 0, A.Length - 1);
 
             if (A != null)
             {
@@ -18,47 +18,34 @@ namespace QuickSort
 
         private static void Quicksorting(int[] a, int p, int r)
         {
-            
-            if (a != null)
+            if (p < r)
             {
-                Console.WriteLine(String.Join(", ", a));
-            }
-            if (p<r)
-            {
-                //Console.WriteLine(Convert.ToInt32(Partition(a, p, r)));
                 int q = Partition(a, p, r);
-
-                Console.WriteLine(q);
                 Quicksorting(a, p, q - 1);
-                Console.WriteLine("sort left");
-                //if (a != null)
-                //{
-                //    Console.WriteLine(String.Join(", ", a));
-                //}
                 Quicksorting(a, q + 1, r);
             }
-
         }
 
         private static int Partition(int[] a, int p, int r)
         {
-            int x = a[r];
+            int x = a[r];    //A[r] pivol is the last element of the array
             int i = p - 1;
-            for (int j = p; j < r - 1; j++)
+
+            for (int j = p; j <= r - 1; j++)
             {
                 if (a[j] <= x)
                 {
-                    i++;
+                    i = i + 1;
                     int temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
-                    j++;
                 }
-                //Console.WriteLine(i);
-                int temp1 = a[i];
-                a[i] = a[r];
-                a[r] = temp1;
             }
+
+            int temp1 = a[i + 1];
+            a[i + 1] = a[r];
+            a[r] = temp1;
+
             return i + 1;
         }
     }
